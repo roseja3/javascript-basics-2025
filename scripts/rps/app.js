4fe3f932d6aeaf71503c174b1c7e08aca245c5d8
Robert Hardy <rhardy@dcsdk12.org>
2025-09-03 19:01:54 -0600

    Add Rock Paper Scissors files.

---
+++ scripts/rps/app.js
@@ -0,0 +1,11 @@
+//@ts-check
+
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+let aftermathElement = document.getElementById("aftermath");
+
+const pickWeapon = function (weapon) {
+	console.log("Player picked", weapon);
+};
