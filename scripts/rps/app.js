615107219ef3af7d233198bdff140e9d73da346d
Robert Hardy <rhardy@dcsdk12.org>
2025-09-08 15:30:45 -0600

    Add random computer weapon selection

--- scripts/rps/app.js
+++ scripts/rps/app.js
@@ -1,5 +1,11 @@
 //@ts-check
 
+// 1. player picks a weapon
+// 2. Computer pick random weapon
+// 3. Determine winner
+// 4. Display
+// 5. Play again / keep playing
+
 const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
@@ -8,4 +14,18 @@ let aftermathElement = document.getElementById("aftermath");
 
 const pickWeapon = function (weapon) {
 	console.log("Player picked", weapon);
+};
+
+const selectComputerWeapon = function () {
+	const rand = Math.floor(Math.random() * 3);
+
+	if (rand == 0) {
+		return ROCK;
+	}
+
+	if (rand == 1) {
+		return PAPER;
+	}
+
+	return SCISSORS;
 };
